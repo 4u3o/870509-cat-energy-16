411146e0191432257426f8b15243139e24970ff9
4u3o <radjahair@gmail.com>
2019-05-15 21:57:01 +1000

    js

---
+++ build/js/menu.js
@@ -0,0 +1,24 @@
+var link = document.querySelector(".page-header__toggle");
+var menu = document.querySelector(".main-nav");
+
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if (link.classList.contains("page-header__toggle--opened")) {
+    link.classList.remove("page-header__toggle--opened");
+    menu.classList.remove("main-nav--opened");
+  } else {
+    link.classList.add("page-header__toggle--opened");
+    menu.classList.add("main-nav--opened");
+  }
+});
+
+
+document.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (menu.classList.contains("main-nav--opened")) {
+      menu.classList.remove("main-nav--opened");
+    }
+  }
+});
