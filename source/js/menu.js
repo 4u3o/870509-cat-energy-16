a1d6b6d5c4eb8333270051e078b86a1db83a117d
4u3o <radjahair@gmail.com>
2019-05-22 13:09:44 +1000

    Карта без JS

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,5 +1,14 @@
 var link = document.querySelector(".page-header__toggle");
 var menu = document.querySelector(".main-nav");
+var staticMap = document.querySelector(".map__static");
+var pin = document.querySelector(".map__pin");
+var yaMap = document.querySelector(".map__js");
+
+if (yaMap.classList.contains("map__js--disabled")) {
+  yaMap.classList.remove("map__js--disabled");
+  pin.classList.add("map__pin--disabled");
+  staticMap.classList.add("map__pin--disabled");
+}
 
 link.classList.remove("page-header__toggle--hidden");
 menu.classList.remove("main-nav--opened");
